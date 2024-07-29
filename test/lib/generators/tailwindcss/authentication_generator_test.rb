acdd45ab09de6b54474be8f73f4e02ad2e7f6f0e
Mike Dalessio <mike.dalessio@gmail.com>
2024-07-29 13:26:30 -0400

    introduce specialized view for rails 8 authentication generator

---
+++ test/lib/generators/tailwindcss/authentication_generator_test.rb
@@ -0,0 +1,13 @@
+require "test_helper"
+require "generators/tailwindcss/authentication/authentication_generator"
+
+class Tailwindcss::Generators::AuthenticationGeneratorTest < Rails::Generators::TestCase
+  tests Tailwindcss::Generators::AuthenticationGenerator
+  destination TAILWINDCSS_TEST_APP_ROOT
+  
+  test "generates the new session template" do
+    run_generator
+
+    assert_file "app/views/sessions/new.html.erb"
+  end
+end
